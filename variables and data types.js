bd0e4aa3bdf4f4ee53413870123234876ba4629f
himanshu-tiwari123 <158790063+himanshu-tiwari123@users.noreply.github.com>
2024-05-18 16:38:21 +0000

    variables and data types

---
+++ variables and data types.js
@@ -0,0 +1,39 @@
+const accountId = 144553
+let accountEmail = "himanshu@google.com"
+var accountPassword = "123456789"
+accountCity="Hyderabad"
+//const  means it cant be changed
+
+// accountId = 2
+console.log(accountId)
+
+let accountState;  //iske bare me koi jaankari uplabdh nhi hai
+
+console.table([accountId , accountEmail, accountPassword, accountCity,accountState]);
+
+/*
+In javascript it is recommended not to use var instead use let ,cause of certain scope issues
+*/
+
+"use strict";
+
+//Alert
+// alert(3+3)  . We are using node , hence there will be no alert
+
+//Data types 
+let name="Himanshu Tiwari"
+let age = 18
+let isLoggedIn = true;
+//Number => range 2 to power 53 even bigger ke liye we use big int
+
+/*null => standalone Value;
+undefined => value has not been assigned
+symbol => used to find uniqueness
+*/
+
+console.log(typeof(null))   //Note null is a type of object
+console.log(typeof(undefined)) //typeof(undefined) is undefined
+
+
+
+
